3db47de914f6b04c52f9f84cfe885a50c72207ef
Dylan DiBeneditto <dibenedittod@gmail.com>
2024-08-24 16:27:12 -0400

    first commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static void main(String[] args) {
+        clearScreen();
+        System.out.println(ANSI_GREEN + "I'm green" + ANSI_RESET);
+    }
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+}
