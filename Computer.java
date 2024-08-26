b4b3d8b19dae8bdb09256f6e921b79ae4a7fc996
Dylan DiBeneditto <dibenedittod@gmail.com>
2024-08-25 22:40:53 -0400

    fix: git error

--- Computer.java
+++ Computer.java
@@ -1,24 +1,80 @@
-public int minimax(Board board, int depth, boolean isMax) {
-    int score = board.evaluate(turn);
+public class Computer extends Player {
 
-    // If the computer has won the game
-    if (score == 1) {
-        return score - depth; // Subtract depth to prioritize quicker wins
+    public Computer(Boolean playerTurn) {
+        super(playerTurn);
     }
 
-    // If the opponent has won the game
-    if (score == -1) {
-        return score + depth; // Add depth to prioritize delaying loss
+    public int minimax(Board board, int depth, boolean isMax) {
+        int score = board.evaluate(turn);
+    
+        // If the computer has won the game
+        if (score == 1) {
+            return score - depth; // Subtract depth to prioritize quicker wins
+        }
+    
+        // If the opponent has won the game
+        if (score == -1) {
+            return score + depth; // Add depth to prioritize delaying loss
+        }
+    
+        // If there are no moves left and no winner
+        if (!board.isMovesLeft()) {
+            return 0;
+        }
+    
+        // Maximizer's move (Computer)
+        if (isMax) {
+            int best = Integer.MIN_VALUE;
+    
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    // Check if the cell is empty
+                    if (!board.mask[i][j]) {
+                        // Make the move
+                        board.mask[i][j] = true;
+                        board.side[i][j] = turn;
+    
+                        // Call minimax recursively and choose the maximum value
+                        best = Math.max(best, minimax(board, depth + 1, false));
+    
+                        // Undo the move
+                        board.mask[i][j] = false;
+                        board.side[i][j] = null;
+                    }
+                }
+            }
+            return best;
+        }
+    
+        // Minimizer's move (Opponent)
+        else {
+            int best = Integer.MAX_VALUE;
+    
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    // Check if the cell is empty
+                    if (!board.mask[i][j]) {
+                        // Make the move
+                        board.mask[i][j] = true;
+                        board.side[i][j] = !turn;
+    
+                        // Call minimax recursively and choose the minimum value
+                        best = Math.min(best, minimax(board, depth + 1, true));
+    
+                        // Undo the move
+                        board.mask[i][j] = false;
+                        board.side[i][j] = null;
+                    }
+                }
+            }
+            return best;
+        }
     }
 
-    // If there are no moves left and no winner
-    if (!board.isMovesLeft()) {
-        return 0;
-    }
-
-    // Maximizer's move (Computer)
-    if (isMax) {
-        int best = Integer.MIN_VALUE;
+    // This will be called to find the best move for the computer
+    public int[] findBestMove(Board board) {
+        int bestVal = Integer.MIN_VALUE;
+        int[] bestMove = { -1, -1 };
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -28,39 +84,29 @@ public int minimax(Board board, int depth, boolean isMax) {
                     board.mask[i][j] = true;
                     board.side[i][j] = turn;
 
-                    // Call minimax recursively and choose the maximum value
-                    best = Math.max(best, minimax(board, depth + 1, false));
+                    // Compute evaluation function for this move
+                    int moveVal = minimax(board, 0, false);
 
                     // Undo the move
                     board.mask[i][j] = false;
                     board.side[i][j] = null;
+
+                    // If the value of the current move is more than the best value, update best
+                    if (moveVal > bestVal) {
+                        bestMove[0] = i;
+                        bestMove[1] = j;
+                        bestVal = moveVal;
+                    }
                 }
             }
         }
-        return best;
+        return bestMove;
     }
 
-    // Minimizer's move (Opponent)
-    else {
-        int best = Integer.MAX_VALUE;
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                // Check if the cell is empty
-                if (!board.mask[i][j]) {
-                    // Make the move
-                    board.mask[i][j] = true;
-                    board.side[i][j] = !turn;
-
-                    // Call minimax recursively and choose the minimum value
-                    best = Math.min(best, minimax(board, depth + 1, true));
-
-                    // Undo the move
-                    board.mask[i][j] = false;
-                    board.side[i][j] = null;
-                }
-            }
-        }
-        return best;
+    // Override makeMove to use the best move found by minimax
+    @Override
+    public Board makeMove(Board board, int x, int y) {
+        int[] bestMove = findBestMove(board);
+        return super.makeMove(board, bestMove[0], bestMove[1]);
     }
 }
